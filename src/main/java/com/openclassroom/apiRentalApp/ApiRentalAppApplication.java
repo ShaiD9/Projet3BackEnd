886fed826067575b273c746a507cbab68c3771ec
ShaiD9 <fffdiamant.orange@gmail.com>
2024-11-06 15:05:24 +0100

    inital commit

---
+++ src/main/java/com/openclassroom/apiRentalApp/ApiRentalAppApplication.java
@@ -0,0 +1,13 @@
+package com.openclassroom.apiRentalApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRentalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRentalAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openclassroom/apiRentalApp/ApiRentalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassroom.apiRentalApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRentalAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
