e9b9d55b0096d3fcffea412975e38a4e9bc6154b
ShaiD9 <fffdiamant.orange@gmail.com>
2024-11-20 11:11:34 +0100

    fix variable name

--- src/main/java/com/openclassroom/apiRentalApp/repositories/UserRepository.java
+++ src/main/java/com/openclassroom/apiRentalApp/repositories/UserRepository.java
@@ -5,5 +5,5 @@ import com.openclassroom.apiRentalApp.models.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByName(String Name);
-    User findByEmail(String Name);
+    User findByEmail(String Email);
 }
